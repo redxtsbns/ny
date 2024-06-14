2955eedb3845185c35cf4057994a10f1a504c91f
redxtsbns <c06971226@gmail.com>
2024-06-14 22:17:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,71 @@
-const express = require('express')
-const app = express()
-////////
-const fs = require('fs')
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+let comments = []; // Initialize an empty array to hold comments
+
+// Read comments from comments.json initially
 fs.readFile('comments.json', (err, data) => {
-  if (err) return console.log(err)
-  const c = JSON.parse(data)
-})
-app.use('/show', (req, res) => {
-  res.send(c)
-})
-app.use('/send', (req, res) => {
-  const name = req.query.name
-  const comment = req.query.comment
-  const date = new Date();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const year = date.getFullYear();
-  const data = {
-    name: name,
-    comment: comment,
-    date: `${month}-${day}-${year}`
+  if (err) {
+    console.log(err);
+  } else {
+    comments = JSON.parse(data); // Parse existing comments if the file exists
   }
+});
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Endpoint to show comments
+app.get('/show', (req, res) => {
+  res.json(comments);
+});
+
+// Endpoint to receive and store new comment
+app.post('/send', (req, res) => {
+  const { name, comment } = req.body;
   if (!name || !comment) {
-    res.send('Your Name/Comment is undefined:(')      
+    res.status(400).send('Your Name/Comment is undefined :(');
   } else {
-    fs.writeFile('comments.json', JSON.stringify(data, null, 2), (err) => {
-      if (err) return res.status(200).send("Failed to send")
-      res.send("Success Send!")
-    })
-})
-app.use('/script', (req, res) => {
-  const key = req.query.key
-  const x = JSON.parse(fs.readFileSync('k.json'))
-  if (key in x.key) {
-    if (x.type === "Free") return res.status(200).send(fs.readFileSync('./src/free-script.lua')
-    if (x.type === "VIP") return res.status(200).send(fs.readFileSync('./src/vip_script-TSBNS.lua')
-                                                      
-  } else {
-    res.send("Key is Invalid!")
+    const date = new Date();
+    const newComment = {
+      name: name,
+      comment: comment,
+      date: `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`
+    };
+    comments.push(newComment); // Add new comment to the array
+
+    // Write updated comments array to comments.json
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 2), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Failed to send');
+      } else {
+        res.send('Success Send!');
+      }
+    });
   }
-})
+});
+
+// Endpoint to serve different scripts based on key type
+app.get('/script', (req, res) => {
+  const key = req.query.key;
+  const x = JSON.parse(fs.readFileSync('k.json'));
+
+  if (key in x) {
+    if (x[key].type === 'Free') {
+      res.status(200).send(fs.readFileSync('./src/free-script.lua'));
+    } else if (x[key].type === 'VIP') {
+      res.status(200).send(fs.readFileSync('./src/vip_script-TSBNS.lua'));
+    } else {
+      res.status(400).send('Unknown script type');
+    }
+  } else {
+    res.status(400).send('Key is Invalid!');
+  }
+});
+
+// Start the server
 app.listen(5000, () => {
-  console.log("Running!!")
-})
+  console.log('Running on port 5000');
+});
